63c5e2be976ddbf4fc763d70fefdec88b89717dc
Murat Knecht <murat@notebook7479.(none)>
2009-08-09 16:42:46 +0200

    Prepared final build v1.0-alpha4

--- src/java/net/sf/profiler4j/agent/AgentConstants.java
+++ src/java/net/sf/profiler4j/agent/AgentConstants.java
@@ -21,7 +21,7 @@ package net.sf.profiler4j.agent;
  */
 public class AgentConstants {
 
-    public static final String VERSION = "1.0-alpha3";
+    public static final String VERSION = "1.0-alpha4";
     
     public static final int CMD_GC = 1;
     public static final int CMD_SNAPSHOT = 2;
