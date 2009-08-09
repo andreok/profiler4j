a8e411f889cdd469ebd0fd0f2bb93f419cf8a25a
Murat Knecht <murat@notebook7479.(none)>
2009-08-09 14:47:06 +0200

    cleaning up, removed debug code

--- src/java/net/sf/profiler4j/console/Project.java
+++ src/java/net/sf/profiler4j/console/Project.java
@@ -28,9 +28,7 @@ import org.apache.commons.lang.SystemUtils;
 public class Project {
 
     /** Specifies that test classes from the profiler4j project are to be instrumented and profiled. */
-//    private static final Rule RULE__ACCEPT_PROFILER4J_TEST_CLASSES = new Rule("net.sf.profiler4j.test.*(*)", Rule.Action.ACCEPT);
-    // TODO (mk) remove me on project finish
-    private static final Rule RULE__ACCEPT_PROFILER4J_TEST_CLASSES = new Rule("org.gudy.azureus2.ui.*(*)", Rule.Action.ACCEPT);
+    private static final Rule RULE__ACCEPT_PROFILER4J_TEST_CLASSES = new Rule("net.sf.profiler4j.test.*(*)", Rule.Action.ACCEPT);
     private final static String EXPORT_PATTERN_DEFAULT = SystemUtils.getUserHome() + SystemUtils.FILE_SEPARATOR + "profiler4j_%i.png"; 
 
     private boolean changed;
